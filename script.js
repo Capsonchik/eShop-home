d0fec7fdb89ee9c98e1d38c225e7978624bda5c7
Capsonchik <Serega00790@mail.ru>
2021-05-14 15:20:37 +0300

    witout comments

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// const goods = [
-//     { title: 'Shirt', price: 150, },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-//   ];
-  
-//   const renderGoodsItem = (title, price) => {
-//     return `<div class="goods-item">
-//             <img src="https://via.placeholder.com/150x200" alt="test">
-//             <h3>${title}</h3>
-//             <p>${price}</p></div>`;
-//   };
-  
-//   const renderGoodsList = (list) => {
-//     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-//     document.querySelector('.goods-list').innerHTML = goodsList;
-//   }
-  
-//   renderGoodsList(goods);
-
 const goods = [
     { title: 'Shirt', price: 150, },
     { title: 'Socks', price: 50 },
@@ -27,23 +6,15 @@ const goods = [
     
   ];
 
-// const renderGoodsItem = (title, price) => {
-//     return `<div class="goods-item">
-//             <img src="https://via.placeholder.com/150x200" alt="test">
-//             <h3>${title}</h3>
-//             <p>${price}</p></div>`;
-//   };
+/* Функция которая добавляет товары на страницу из GOODS */  
 
-//   goods.forEach (function (el) {
-//       document.querySelector('.goods-list').insertAdjacentHTML('afterbegin', `<div class="goods-item">
-//       <img src="https://via.placeholder.com/150x200" alt="test">
-//       <h3>${el.title}</h3>
-//       <p>${el.price}</p></div>`)
-//   })
-
-goods.forEach ((el) => {
-    document.querySelector('.goods-list').insertAdjacentHTML('afterbegin', `<div class="goods-item">
-      <img src="https://via.placeholder.com/150x200" alt="test">
-      <h3>${el.title}</h3>
-      <p>${el.price}</p></div>`)
-})
+const renderGoodsList = () => {
+    goods.forEach ((el) => {
+        document.querySelector('.goods-list').insertAdjacentHTML('afterbegin', `<div class="goods-item">
+          <img src="https://via.placeholder.com/150x200" alt="test">
+          <h3>${el.title}</h3>
+          <p>${el.price}</p></div>`)
+        })
+    };
+      
+renderGoodsList();
