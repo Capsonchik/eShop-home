b309654e4c2ef602043f9f10d1d497c3bbfebed5
Capsonchik <Serega00790@mail.ru>
2021-05-24 16:17:37 +0300

    new cart

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-// const goods = [
-//     { title: 'Shirt', price: 150, },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-    
-//   ];
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
 
-// /* Функция которая добавляет товары на страницу из GOODS */  
+const makeGETRequest = (url, callback) => {
+  return new Promise((resolve, reject) => {
+    let xhr;
 
-// const renderGoodsList = () => {
-//     goods.forEach ((el) => {
-//         document.querySelector('.goods-list').insertAdjacentHTML('afterbegin', `<div class="goods-item">
-//           <img src="https://via.placeholder.com/150x200" alt="test">
-//           <h3>${el.title}</h3>
-//           <p>${el.price}</p></div>`)
-//         })
-//     };
-      
-// renderGoodsList();
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { 
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) callback(xhr.responseText)
+        else reject('Error')
+      }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send();
+  })
+}
 
 class GoodsItem {
-  constructor(title, price) {
-    this.title = title;
+  constructor(product_name, price) {
+    this.product_name = product_name;
     this.price = price;
   }
 
   render() {
-    return `<div class="goods-item"><img src="https://via.placeholder.com/150x200" alt="test"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    return `<div class="goods-item"><img src="https://via.placeholder.com/150x200" alt="test"><h3>${this.product_name}</h3><p>${this.price}</p><button class="push-cart">test</button></div>`;
   }
 }
 
@@ -35,73 +38,91 @@ class GoodsList {
     this.goods = [];
   }
 
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+  fetchGoods(cb) {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    })
   }
 
   render() {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
   }
 
-
-  //  Вроде все работает, запихнуть это дело в корзину с методом рендер
-  itemsSum() {
-     let result = 0;
-     this.goods.forEach(el => {
-       let sum = el.price
-       result += sum
-     })
-     document.querySelector('.test-sum').innerHTML = result;  
+  getTotalSum() {
+    const totalSum = this.goods.reduce((acc, item) => {
+      if (!item.price) return acc;
+      return acc + item.price;
+    }, 0);
+    console.log(totalSum);
   }
 }
-  class renderCart {
-    constructor(){
-
-    }
-    remooveItem(){
-
-    }
-
-    addItem() {
-
-    }
-
-    render() {
-
-    }
-
-    sort() {
-
-    }
-
-  }
-
-  class cartItem {
-    constructor(){
-      
-    }
-    changeSum() {
-
-    }
-  }
-
-
-
-
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.itemsSum();
-console.log()
+list.fetchGoods(() => {
+  list.render();
+});
+
+
+
+class Basket extends GoodsList {
+  constructor(...args) {
+    super(...args);
+    this.cartItems = [];
+  }
+
+  
+
+  clearAll() {
+
+  }
+  // Сделать отдельный массив с товарами корзины
+  //! 1.По клику нужно как то получить обьект в котором кнопка
+  // кстати как вариант сделать кнопку отдельно в блоке и получать его парент ноду и ее уже кидать в массив ( но это не точно)
+  //! 2.Затем запушить обьект в массив
+  //! 3. Разметку сформировать так, чтобы в отдельном поле можно было выводить итоговую сумму корзины ( метод уже есть, либо через фор ич его пересчитать) 
+  //! 4. В целом не сложно но КАК??????? 
+  addItem() {
+    document.onclick = event => {
+      if (event.target.classList.contains('push-cart')){
+        this.cartItems.push
+      }
+      console.log(this.cartItems)
+    }
+    
+
+  }
+
+  removeItem() {
+
+  }
+
+  render() {
+    return `<div class="goods-item"><img src="https://via.placeholder.com/150x200" alt="test"><h3>${this.product_name}</h3><p>${this.price}</p><button>test</button></div>`;
+  }
+
+}
+
+// не забыть убрать 
+console.log(list)
+
+
+const cart = new Basket()
+cart.addItem()
+
+class BasketItem extends GoodsItem {
+  constructor(...args) {
+    super(...args);
+    this.count = 0;
+  }
+
+  addOne() {}
+
+  removeOne() {}
+}
 
